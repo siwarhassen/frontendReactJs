fab5336db0c7d2db4630308979cbc389cab359b8
siwarhassen <49081647+siwarhassen@users.noreply.github.com>
2021-05-12 22:34:09 +0100

    Update useChat.js

--- src/user/components/Chat/useChat.js
+++ src/user/components/Chat/useChat.js
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import socketIOClient from "socket.io-client";
 
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
-const SOCKET_SERVER_URL = "https://aaweni.herokuapp.com:4000";
+const SOCKET_SERVER_URL = "https://aaweni.herokuapp.com";
 
 const useChat = (roomId) => {
   const [messages, setMessages] = useState([]);
