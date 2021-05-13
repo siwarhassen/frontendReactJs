9f1de9f8869a7bde1de38e25e631200fe4d28fd8
siwarhassen <49081647+siwarhassen@users.noreply.github.com>
2021-05-13 11:11:36 +0100

    Update SocketContext.js

--- src/user/components/videoChat/contexts/SocketContext.js
+++ src/user/components/videoChat/contexts/SocketContext.js
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 export const SocketIoContext = createContext();
 
 const SocketContext = ({ children }) => {
-  const socket = io.connect("ws://test.herokuapp.com/socket.io/?EIO=4&transport=websocket");
+  const socket = io.connect("https://aaweni.herokuapp.com/socket.io/?EIO=4&transport=websocket");
   console.log(socket);
   return (
     <SocketIoContext.Provider value={{ socket }}>
