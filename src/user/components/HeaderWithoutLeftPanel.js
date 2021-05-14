8c57bbf3e3b7eaeefc5300e556b9581170e05ad3
siwarhassen <49081647+siwarhassen@users.noreply.github.com>
2021-05-14 22:58:04 +0100

    Update HeaderWithoutLeftPanel.js

--- src/user/components/HeaderWithoutLeftPanel.js
+++ src/user/components/HeaderWithoutLeftPanel.js
@@ -36,14 +36,14 @@ const [userId, setUserId] = useState("");
                     }
                 }
                 try {
-                    axios.get("/api/auth/details_user",config).then((response)=>{
+                    axios.get("https://aaweni.herokuapp.com/api/auth/details_user",config).then((response)=>{
                         setUser(response.data.data);
                         console.log(user)});                    
                    
                 } catch (error) {
                     console.log(error)
                 }
-                axios.get(`/notif/getAll`, config)
+                axios.get(`https://aaweni.herokuapp.com/notif/getAll`, config)
                 .then((response) => {
                 setNotif(response.data);
                 console.log(response.data)
@@ -65,7 +65,8 @@ const [userId, setUserId] = useState("");
           
 
 <div>
-   <header style={{zIndex:100}}>
+
+  <header style={{zIndex:100}}>
      
             <div class="header_wrap">
                 <div class="header_inner mcontainer">
@@ -76,10 +77,10 @@ const [userId, setUserId] = useState("");
                         </span>
 
                         <div id="logo">
-                            <a href="homepage.html"> 
+                            <Link to={"/posts"}> 
                                 <img src="assets/user/images/logoo.png" alt=""/>
                                 <img src="assets/user/images/logoo.png" class="logo_mobile" alt="" />
-                            </a>
+                            </Link>
                         </div>
                     </div>
                      {/*  <!-- search icon for mobile -->*/}
@@ -124,116 +125,11 @@ const [userId, setUserId] = useState("");
                     <div class="right_side">
     
                         <div class="header_widgets">
-                            <a href="#" class="is_link">  Home  </a> 
-                            <a href="#" class="is_link">  Dennis Han  </a>  
-                           
-                            <div uk-drop="mode: click" class="header_dropdown dropdown_cart">
-                             
-                                <div class="drop_headline">
-                                    <h4>  My Cart </h4>
-                                    <a href="#" class="btn_action hover:bg-gray-100 mr-2 px-2 py-1 rounded-md underline"> Checkout </a>
-                                </div>
-
-                                <ul class="dropdown_cart_scrollbar" data-simplebar>
-                                    <li>
-                                        <div class="cart_avatar">
-                                            <img src="assets/user/images/product/2.jpg" alt=""/>
-                                        </div>
-                                        <div class="cart_text">
-                                            <div class=" font-semibold leading-4 mb-1.5 text-base line-clamp-1"> Wireless headphones </div>
-                                            <p class="text-sm">Type Accessories  </p>
-                                        </div>
-                                        <div class="cart_price">
-                                            <span> $14.99 </span>
-                                            <button class="type"> Remove</button>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="cart_avatar">
-                                            <img src="assets/user/images/product/13.jpg" alt=""/>
-                                        </div>
-                                        <div class="cart_text">
-                                            <div class=" font-semibold leading-4 mb-1.5 text-base line-clamp-1"> Parfum Spray</div>
-                                            <p class="text-sm">Type Parfums  </p>
-                                        </div>
-                                        <div class="cart_price">
-                                            <span> $16.99 </span>
-                                            <button class="type"> Remove</button>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="cart_avatar">
-                                            <img src="assets/user/images/product/15.jpg" alt=""/>
-                                        </div>
-                                        <div class="cart_text">
-                                            <div class=" font-semibold leading-4 mb-1.5 text-base line-clamp-1"> Herbal Shampoo </div>
-                                            <p class="text-sm">Type Herbel  </p>
-                                        </div>
-                                        <div class="cart_price">
-                                            <span> $12.99 </span>
-                                            <button class="type"> Remove</button>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="cart_avatar">
-                                            <img src="assets/user/images/product/14.jpg" alt=""/>
-                                        </div>
-                                        <div class="cart_text">
-                                            <div class=" font-semibold leading-4 mb-1.5 text-base line-clamp-1"> Wood Chair </div>
-                                            <p class="text-sm">Type Furniture  </p>
-                                        </div>
-                                        <div class="cart_price">
-                                            <span> $19.99 </span>
-                                            <button class="type"> Remove</button>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="cart_avatar">
-                                            <img src="assets/user/images/product/9.jpg" alt=""/>
-                                        </div>
-                                        <div class="cart_text">
-                                            <div class=" font-semibold leading-4 mb-1.5 text-base line-clamp-1"> Strawberries FreshRipe </div>
-                                            <p class="text-sm">Type Fruit  </p>
-                                        </div>
-                                        <div class="cart_price">
-                                            <span> $12.99 </span>
-                                            <button class="type"> Remove</button>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="cart_avatar">
-                                            <img src="assets/user/images/product/2.jpg" alt=""/>
-                                        </div>
-                                        <div class="cart_text">
-                                            <div class=" font-semibold leading-4 mb-1.5 text-base line-clamp-1"> Wireless headphones </div>
-                                            <p class="text-sm">Type Accessories  </p>
-                                        </div>
-                                        <div class="cart_price">
-                                            <span> $14.99 </span>
-                                            <button class="type"> Remove</button>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="cart_avatar">
-                                            <img src="assets/user/images/product/13.jpg" alt=""/>
-                                        </div>
-                                        <div class="cart_text">
-                                            <div class=" font-semibold leading-4 mb-1.5 text-base line-clamp-1"> Parfum Spray</div>
-                                            <p class="text-sm">Type Parfums  </p>
-                                        </div>
-                                        <div class="cart_price">
-                                            <span> $16.99 </span>
-                                            <button class="type"> Remove</button>
-                                        </div>
-                                    </li>
-                                </ul>
-        
-                                <div class="cart_footer">
-                                    <p> Subtotal : $ 320 </p>
-                                    <h1> Total :  <strong> $ 320</strong> </h1>
-                                </div>
-                            </div>
-
+                        <Link to={"/posts"} class="is_link"> 
+                               Home
+                            </Link>
+                          
+                        
                             <a href="#" class="is_icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -276,8 +172,8 @@ const [userId, setUserId] = useState("");
                             </div> 
 
                               {/*<!-- Message --> */}
-                                <Link to={`/chat/${user?._id}`}  >                     
-                                       <a href="#" class="is_icon">
+                              <Link to={`/chat/${user._id}`}  >
+                            <a href="#" class="is_icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
                                 </svg>
@@ -285,24 +181,24 @@ const [userId, setUserId] = useState("");
                             </a>
                             </Link>
                     
-                            <a href="#">
-                                <img src={user?.profilePicture} class="is_avatar" alt=""/>
-                            </a>
+                            <Link to="/postsprofile">
+                                <img src={user.profilePicture} class="is_avatar" alt=""/>
+                            </Link>
                             <div uk-drop="mode: click;offset:5" class="header_dropdown profile_dropdown">
 
-                                <a href="timeline.html" class="user">
+                                <Link to="/postsprofile" class="user">
                                     <div class="user_avatar">
-                                        <img src={user?.profilePicture} alt=""/>
+                                        <img src={user.profilePicture} alt=""/>
                                     </div>
                                     <div class="user_name">
-                                        <div> {user?.username} </div>
-                                        <span> {user?.email} </span>
+                                        <div> {user.username} </div>
+                                        <span> {user.email} </span>
                                     </div>
-                                </a>
+                                </Link>
                                 <hr class="border-gray-100"/>
                                 <a>
                                     <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path></svg>
-                                    <Link to="/profile_education">My Account </Link>
+                                    <Link to="/postsprofile">My Account </Link>
                                     
                                 </a>
                                 <a>
@@ -312,19 +208,8 @@ const [userId, setUserId] = useState("");
                                     <Link to="/mypages" class="lg:px-2">Manage Pages</Link>
                                     
                                 </a>
-                                <a href="group-feed.html">
-                                    <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"></path><path fill-rule="evenodd" d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z" clip-rule="evenodd"></path></svg>
-                                    My Billing 
-                                </a>
-                                <a href="#" id="night-mode" class="btn-night-mode">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-                                      </svg>
-                                     Night mode
-                                    <span class="btn-night-mode-switch">
-                                        <span class="uk-switch-button"></span>
-                                    </span>
-                                </a>
+                              
+                           
                                 <a>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path>
@@ -344,18 +229,10 @@ const [userId, setUserId] = useState("");
   </header>
 
 
-
-       
-
+     
 
         </div>
-
-
-     
-   )
-    
+ )
 }
-
-
 
 export default HeaderWithoutLeftPanel;
