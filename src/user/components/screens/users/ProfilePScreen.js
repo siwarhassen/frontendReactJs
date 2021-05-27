9d0109141034989ae5ceaf4b7df2bdbcca0b5d11
siwarhassen <49081647+siwarhassen@users.noreply.github.com>
2021-05-27 02:12:10 +0100

    Update ProfilePScreen.js

--- src/user/components/screens/users/ProfilePScreen.js
+++ src/user/components/screens/users/ProfilePScreen.js
@@ -314,7 +314,8 @@ const id = match.params.id;
          </div>
          <div class="card-body">
              <h5 class="card-title">{val?.name}</h5>
-             <p class="card-text"><strong>Url Project:</strong> {val?.urlProject}</p>
+               <p class="card-text"><strong>Url Project:</strong> <a target="_blank" class="card-text" rel="noopener noreferrer" href={val?.urlProject}>{val?.urlProject}</a> </p>
+          
              <p class="card-text"><strong>Description:</strong> {val?.description}</p>
          </div>
      </div> 
