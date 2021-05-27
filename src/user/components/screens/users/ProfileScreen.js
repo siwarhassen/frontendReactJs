9a0d109b7df0c015ae6c6fb20c6a4a3b5996ea15
siwarhassen <49081647+siwarhassen@users.noreply.github.com>
2021-05-27 01:03:45 +0100

    Update ProfileScreen.js

--- src/user/components/screens/users/ProfileScreen.js
+++ src/user/components/screens/users/ProfileScreen.js
@@ -419,7 +419,7 @@ const id = match.params.id;
                               </li>
                               <li class="flex items-center space-x-2"> 
                                   <ion-icon name="logo-rss"  class=" rounded-full bg-gray-200 text-xl p-1.5 mr-3"></ion-icon>
-                                  Flowwed By <strong> {numbersFby} Peaple </strong>
+                                  Followed By <strong> {numbersFby} Peaple </strong>
                               </li>                                
                           </ul>
                           
