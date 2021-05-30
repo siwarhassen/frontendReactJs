0e2fddcda5f1914fa3a3809b162c0c677991b1eb
siwarhassen <49081647+siwarhassen@users.noreply.github.com>
2021-05-31 00:29:00 +0100

    Update HeaderWithoutLeftPanel.js

--- src/user/components/HeaderWithoutLeftPanel.js
+++ src/user/components/HeaderWithoutLeftPanel.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import axios from 'axios';
 
 import CreateRoom from "./videoChat/components/modal/CreateModal";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
 
 import JoinRoom from './videoChat/components/modal/Join';
@@ -109,6 +110,9 @@ const [userId, setUserId] = useState("");
                        <button id="searchenter" type="hidden"></button>
                        </Link> 
                     </div>
+                      <button primary large>
+                    <ion-icon name="mic-outline" onMouseDown={SpeechRecognition.startListening}    style={{height:"400px",width:"30px",marginTop:"-10px"}}></ion-icon>
+</button>
                     <div uk-drop="mode: click" class="hidden md:w-1/3 w-11/12 shadow-lg rounded-md -mt-2 bg-white">
                         <div class="-mt-2 p-3">
                             <h4 class="font-semibold mb-1 mt-2 px-2.5 text-lg"> Recently  </h4>
